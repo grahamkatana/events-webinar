ecf7d050d95fe1d72151b901f1c0c1d3295746a5
grahamkatana <graykatanakenny@gmail.com>
2022-08-13 19:19:33 +0200

    Added stripe payments and logs

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,8 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :customer
   belongs_to :workshop
 
-  after_create: update_workshop_spot_count
-
+  after_commit :update_workshop_spot_count
+  
   def update_workshop_spot_count
     workshop.update(remaining_spots:workshop.total_spots-no_of_tickets)
   end
--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -41,7 +41,7 @@ class StripeService
 
     def create_stripe_charge(amount_to_be_paid, stripe_customer_id,card_id,workshop)
         Stripe::Charge.create({
-            amount:amount_to_be_paid * 100,
+            amount:amount_to_be_paid.to_i * 100,
             currency:'usd',
             source:card_id,
             customer:stripe_customer_id,
